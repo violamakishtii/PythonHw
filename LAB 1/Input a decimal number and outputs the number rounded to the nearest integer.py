9572903d3c6c6a7654f2924d6da311f97ad8262b
violamakishtii <139384138+violamakishtii@users.noreply.github.com>
2023-07-12 21:54:37 +0200

    Create  Input a decimal number and outputs the number rounded to the nearest integer.py
    Write a Python script that prompts the user to input a decimal number and outputs the number rounded to the nearest integer.

---
+++ LAB 1/Input a decimal number and outputs the number rounded to the nearest integer.py
@@ -0,0 +1,13 @@
+#Write a Python script that prompts the user to input a decimal number and outputs the
+#number rounded to the nearest integer.
+
+def function(x):
+
+    print("The result is:",round(x))
+
+
+
+x = float(input("Input x: "))
+
+
+function(x)
