1d0fb182ea10befe7b1890eb67ba93fe0ce5a15a
violamakishtii <139384138+violamakishtii@users.noreply.github.com>
2023-07-12 20:42:48 +0200

    sumofnumbers.py
    Write a Python program to accept two numbers from the user and display the sum of the two numbers on the screen.

---
+++ sumofnumbers.py
@@ -0,0 +1,11 @@
+#Write a Python program to accept two numbers from the user and display the sum of the
+#two numbers on the screen.
+
+def function(x, y):
+    result = x + y
+    print("The sum is:", result)
+
+x = int(input("Input the first number: "))
+y = int(input("Input the second number: "))
+
+function(x, y)
