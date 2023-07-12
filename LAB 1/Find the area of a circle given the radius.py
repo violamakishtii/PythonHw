cd135265105c700e6a3898a5204ee2693917db23
violamakishtii <139384138+violamakishtii@users.noreply.github.com>
2023-07-12 21:50:40 +0200

    Create Find the area of a circle given the radius.py
    Write a Python script to find the area of a circle given the radius. The value of pi is 3.14159.

---
+++ LAB 1/Find the area of a circle given the radius.py
@@ -0,0 +1,12 @@
+#Write a Python script to find the area of a circle given the radius. The value of pi is 3.14159.
+
+def function(r):
+    area= 3.14159 * r * r
+    print("The area is:",float(area))
+
+
+
+r = float(input("Input radius: "))
+
+
+function(r)
