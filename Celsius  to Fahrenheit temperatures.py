c120c43e1eef0ce05b0e89efd556272d9deaecd6
violamakishtii <139384138+violamakishtii@users.noreply.github.com>
2023-07-12 20:54:35 +0200

    Create Celsius  to Fahrenheit.py
    Write a Python script that converts Celsius temperatures to Fahrenheit temperatures.

---
+++ Celsius  to Fahrenheit temperatures.py
@@ -0,0 +1,11 @@
+#Write a Python script that converts Celsius temperatures to Fahrenheit temperatures.
+
+def function(c):
+    result = 9/5 * c + 32
+   
+    print("The result is:", result)
+    
+
+c = int(input("Input Celsius: "))
+
+function(c)
